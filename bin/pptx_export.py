e853dd2a03a1be144133cedfa1e483b163ca12c8
Noah-Dev <ark_code@protonmail.com>
2017-08-16 06:03:14 -0500

    Tweaks to improve readability

--- bin/pptx_export.py
+++ bin/pptx_export.py
@@ -1,3 +1,5 @@
+# Stop gap solution - I want to implement this with PowerShell
+
 import os
 import sys
 # Retrive the directory of the project - one level up from this file
@@ -5,7 +7,7 @@ dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 # Get the virtualenv python
 python = os.path.join(dir_path, "env/Scripts/python")
 # Entry point of the script
-script = os.path.join(dir_path, "src/watch.py")
+script = os.path.join(dir_path, "src/poll.py")
 
 # Convert args to a string, to be fed into the script
 args = ""
--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ sys.path.insert(0,pdf_path)
 import pdf
 
 # For now, I am just testing whether or not it fails pdf.py files
-# Not sure how to test watch.py yet
+# Not sure how to test watch.py yet. I will figure it out soon enough
 class TestPDF(unittest.TestCase):
 
     def test_standard(self):
